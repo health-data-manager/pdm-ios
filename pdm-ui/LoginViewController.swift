9e9563368fa8416b64d0d57416e143b75d790c43
Dan Potter <dpotter@mitre.org>
2019-04-17 13:33:52 -0400

    Improve the login screen

--- pdm-ui/LoginViewController.swift
+++ pdm-ui/LoginViewController.swift
@@ -10,38 +10,72 @@ import UIKit
 // Displays a login view.
 class LoginViewController: UIViewController {
 
-    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var signInButton: PDMButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        updateButtonState()
     }
 
     @IBAction func loginPressed(_ sender: Any) {
         startLogin()
     }
 
-    func startLogin() {
-        // For now, display the progress indicator and wait...
-        activityIndicator.startAnimating()
-        // Grab the app delegate
-        if let app = UIApplication.shared.delegate as? AppDelegate {
-            if let pdm = app.patientDataManager {
-                pdm.clientLogin(completionHandler: { (error: Error?) -> Void in
-                    DispatchQueue.main.async {
-                        if (error != nil) {
-                            print("An error occurred.")
-                        } else {
-                            self.loginCompleted()
-                        }
-                    }
-                })
-            }
+    @IBAction func emailDone(_ sender: Any) {
+        passwordTextField.becomeFirstResponder()
+    }
+
+    @IBAction func emailChanged(_ sender: Any) {
+        updateButtonState()
+    }
+
+    @IBAction func passwordChanged(_ sender: Any) {
+        updateButtonState()
+    }
+
+    func updateButtonState() {
+        let email = emailTextField.text ?? ""
+        let password = passwordTextField.text ?? ""
+        signInButton.isEnabled = !(email.isEmpty || password.isEmpty)
+    }
+
+    @IBAction func passwordDone(_ sender: Any) {
+        if signInButton.isEnabled {
+            startLogin()
         }
     }
 
+    func startLogin() {
+        // For now, display the progress indicator and wait...
+        // Grab the app delegate
+        guard let app = UIApplication.shared.delegate as? AppDelegate,
+            let pdm = app.patientDataManager,
+            let email = emailTextField.text,
+            let password = passwordTextField.text else {
+                return
+        }
+        let alert = UIAlertController(title: nil, message: "Signing into PDM...", preferredStyle: .alert)
+        let loadingIndicator = UIActivityIndicatorView(frame: CGRect(x: 10, y: 5, width: 50, height: 50))
+        loadingIndicator.style = .gray
+        loadingIndicator.startAnimating()
+        alert.view.addSubview(loadingIndicator)
+        present(alert, animated: true, completion: nil)
+        pdm.user(email, signInWithPassword: password, completionHandler: { (error: Error?) -> Void in
+            DispatchQueue.main.async {
+                self.dismiss(animated: true, completion: nil)
+                if (error != nil) {
+                    print("An error occurred.")
+                } else {
+                    self.loginCompleted()
+                }
+            }
+        })
+    }
+
     func loginCompleted() {
-        activityIndicator.stopAnimating()
         performSegue(withIdentifier: "Login", sender: self)
     }
 }
